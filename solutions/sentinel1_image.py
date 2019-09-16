d9ddcd15a353a12c90902a8f49eb4c5405838ef8
iovsn <jovan.visnjic@sinergise.com>
2019-09-16 17:02:36 +0200

    fixed a bug/typo in sentinel1_image.py

--- solutions/sentinel1_image.py
+++ solutions/sentinel1_image.py
@@ -3,7 +3,7 @@ eopatch = EOPatch.load('../data/sentinel1_sample/', lazy_loading=True)
 
 # create task using data from sample eopatch
 reactiv = ReactivTask((FeatureType.DATA_TIMELESS, 'specle_variability'),
-                      data_feature=(FeatureType.DATA, 'IW_VV'), 
+                      data_feature=(FeatureType.DATA, 'IW_VV'),
                       mask_feature=(FeatureType.MASK, 'IS_DATA'))
 
 # run the task
@@ -12,4 +12,4 @@ eopatch = reactiv.execute(eopatch)
 # plot the results
 from skimage import color
 plt.figure(figsize=(15,15))
-plt.imshow(color.hsv2rgb(eop[(FeatureType.DATA_TIMELESS, 'specle_variability')]))
+plt.imshow(color.hsv2rgb(eopatch[(FeatureType.DATA_TIMELESS, 'specle_variability')]))
