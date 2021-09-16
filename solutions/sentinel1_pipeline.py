e65bb49877a775fc579430274d60f0fae45d5173
maleksandrov <matej.aleksandrov@sinergise.com>
2021-09-16 09:52:37 +0200

    updated sentinel1 notebook and its solutions

--- solutions/sentinel1_pipeline.py
+++ solutions/sentinel1_pipeline.py
@@ -1,17 +1,26 @@
 
 # tasks
-s1_task = S1IWWCSInput('IW_VV', resx='120m', resy='120m',
-                       orbit='descending', time_difference=dt.timedelta(minutes=5))
+s1_task = SentinelHubInputTask(
+    data_collection=DataCollection.SENTINEL1_IW_DES,
+    resolution=(120, 120),
+    bands_feature=(FeatureType.DATA, 'IW_VV'),
+    additional_data=(FeatureType.MASK, 'dataMask', 'IS_DATA'),
+    bands=['VV'],
+    time_difference=dt.timedelta(minutes=5)
+)
 
-reactiv = ReactivTask((FeatureType.DATA_TIMELESS, 'speckle_variability'),
-                      data_feature=(FeatureType.DATA, 'IW_VV'),
-                      mask_feature=(FeatureType.MASK, 'IS_DATA'))
+reactiv_task = ReactivTask(
+    data_feature=(FeatureType.DATA, 'IW_VV'),
+    mask_feature=(FeatureType.MASK, 'IS_DATA'),
+    reactiv_feature=(FeatureType.DATA_TIMELESS, 'speckle_variability')
+)
 
 # workflow
-workflow = LinearWorkflow(s1_task, reactiv)
+workflow = LinearWorkflow(s1_task, reactiv_task)
 
 result = workflow.execute({
     s1_task: {'bbox': bbox, 'time_interval': time_interval}
 })
 
-plot_results(result.eopatch()[(FeatureType.DATA_TIMELESS, 'speckle_variability')])
+image = result.eopatch()[(FeatureType.DATA_TIMELESS, 'speckle_variability')]
+plot_results(image)
--- solutions/sentinel1_task.py
+++ solutions/sentinel1_task.py
@@ -1,13 +1,10 @@
 
 class ReactivTask(EOTask):
 
-    def __init__(self,
-                 reactiv_feature=(FeatureType.DATA_TIMELESS, 'speckle_variability'),
-                 data_feature=(FeatureType.DATA, 'IW_VV'),
-                 mask_feature=(FeatureType.MASK, 'IS_DATA')):
-        self.reactiv_feature = reactiv_feature
+    def __init__(self, data_feature, mask_feature, reactiv_feature):
         self.data_feature = data_feature
         self.mask_feature = mask_feature
+        self.reactiv_feature = reactiv_feature
 
     def execute(self, eopatch):
         eopatch[self.reactiv_feature] = speckle_variability(
