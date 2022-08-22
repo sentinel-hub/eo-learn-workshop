7ab0a4c2eaafbae127e1052e8f7336d468fb30ff
Matej Batič <matej.batic@sinergise.com>
2022-08-22 11:28:38 +0200

    Update to eo-learn 1.2.0, rewritten old eopatches (removing deprecated pickle format and compressed npy files), MIT license.

--- solutions/sentinel1_pipeline.py
+++ solutions/sentinel1_pipeline.py
@@ -1,5 +1,3 @@
-
-# tasks
 s1_task = SentinelHubInputTask(
     data_collection=DataCollection.SENTINEL1_IW_DES,
     resolution=(120, 120),
@@ -8,19 +6,24 @@ s1_task = SentinelHubInputTask(
     bands=['VV'],
     time_difference=dt.timedelta(minutes=5)
 )
+s1_node = EONode(s1_task, inputs=[], name="Get data from SentinelHub")
 
 reactiv_task = ReactivTask(
     data_feature=(FeatureType.DATA, 'IW_VV'),
     mask_feature=(FeatureType.MASK, 'IS_DATA'),
     reactiv_feature=(FeatureType.DATA_TIMELESS, 'speckle_variability')
 )
+reactiv_node = EONode(reactiv_task, inputs=[s1_node], name="Run REACTIV algorith,")
+
+# an extra (output) node to retain in memory the outputs of the workflow
+output_node = EONode(OutputTask("output_eopatch"), inputs=[reactiv_node])
 
 # workflow
-workflow = LinearWorkflow(s1_task, reactiv_task)
+workflow = EOWorkflow(workflow_nodes = (s1_node, reactiv_node, output_node))
 
 result = workflow.execute({
-    s1_task: {'bbox': bbox, 'time_interval': time_interval}
+    s1_node: {'bbox': bbox, 'time_interval': time_interval}
 })
 
-image = result.eopatch()[(FeatureType.DATA_TIMELESS, 'speckle_variability')]
+image = result.outputs["output_eopatch"][(FeatureType.DATA_TIMELESS, 'speckle_variability')]
 plot_results(image)
